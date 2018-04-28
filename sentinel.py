622ddec7e8bd611ff619153abda46aefd9aefb7b
David Ashford <ashford.david@gmail.com>
2018-04-28 20:19:57 +0100

    Add example env

--- sentinel.py
+++ sentinel.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv, find_dotenv
 
 from src.Event.EventDispatcher import EventDispatcher
 from src.Event.Subscriber.TemperatureSubscriber import TemperatureSubscriber
+from src.Event.Subscriber.HumiditySubscriber import HumiditySubscriber
+from src.Event.Subscriber.PressureSubscriber import PressureSubscriber
+from src.Event.Subscriber.AirQualitySubscriber import AirQualitySubscriber
 from src.MQTT.Factory import Factory
 from src.Sensors.Factory import Factory as Device_Factory
 from src.Values.Credentials import Credentials
@@ -18,18 +21,20 @@ if __name__ == '__main__':
 
     event_dispatcher = EventDispatcher()
 
-    temperature_subscriber = TemperatureSubscriber()
-
-    event_dispatcher.add_subscriber(subscriber=temperature_subscriber)
+    event_dispatcher.add_subscriber(subscriber=TemperatureSubscriber())
+    event_dispatcher.add_subscriber(subscriber=HumiditySubscriber())
+    event_dispatcher.add_subscriber(subscriber=PressureSubscriber())
+    event_dispatcher.add_subscriber(subscriber=AirQualitySubscriber())
 
     mqtt_credentials = Credentials(username=os.getenv('MQTT_USERNAME'), password=os.getenv('MQTT_PASSWORD'))
-    mqtt_client = Factory.create_client(provider='paho', credentials=mqtt_credentials)
-    mqtt_client.connect(host='192.168.1.12', port=1883)
+    mqtt_client = Factory.create_client(provider=os.getenv('MQTT_PROVIDER'), credentials=mqtt_credentials)
+    mqtt_client.connect(host=os.getenv('MQTT_HOST'), port=os.getenv('MQTT_PORT'))
 
     scheduler = BackgroundScheduler()
 
     for sensor in configuration['sensors']:
         device = Device_Factory.create_sensor(device=sensor['type'])
+        # TODO come up with better way to add these jobs
         for metric in sensor['metrics']:
             if metric['metric'] == 'temperature':
                 scheduler.add_job(
