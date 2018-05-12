87f11870caf05dec2bc5b3dfb8f1ae7bb6eeb778
David Ashford <ashford.david@gmail.com>
2018-05-12 16:55:04 +0100

    Debug

--- src/Notifications/LED/RGB.py
+++ src/Notifications/LED/RGB.py
@@ -3,11 +3,9 @@ import RPi.GPIO as GPIO
 
 class RGB:
     def __init__(self, configuration):
-        print(configuration)
-        # TODO: don't use indices here
-        self._R = configuration['channels'][0]
-        self._G = configuration['channels'][1]
-        self._B = configuration['channels'][2]
+        self._R = configuration['channels'][0]['r']
+        self._G = configuration['channels'][0]['g']
+        self._B = configuration['channels'][0]['b']
         pins = [
             self._R,
             self._G,
