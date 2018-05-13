976075ae76cff590760e0f961073897b0dedc5ff
David Ashford <ashford.david@gmail.com>
2018-05-13 17:29:10 +0100

    decrease sleep time

--- src/Notification/Subscriber/LED/RGB.py
+++ src/Notification/Subscriber/LED/RGB.py
@@ -24,10 +24,10 @@ class RGB:
         led.start(0)
         for i in range(0, 100):
             led.ChangeDutyCycle(i)
-            time.sleep(0.01)
+            time.sleep(0.005)
         for i in range(100, 0, -1):
             led.ChangeDutyCycle(i)
-            time.sleep(0.01)
+            time.sleep(0.005)
         led.stop()
 
 
