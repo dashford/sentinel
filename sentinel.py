75f7cf0bef9b966080591789e0603350b5a94f2b
David Ashford <ashford.david@gmail.com>
2018-05-13 17:32:33 +0100

    Disable led

--- sentinel.py
+++ sentinel.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         for topic in led['mqtt']['topics']:
             mqtt_client.message_callback_add(subscription=topic['topic'], callback=led_device.blink)
 
-    mqtt_client.subscribe(topic='brompton/living-room/#')
+    # mqtt_client.subscribe(topic='brompton/living-room/#')
 
     scheduler = BackgroundScheduler()
 
