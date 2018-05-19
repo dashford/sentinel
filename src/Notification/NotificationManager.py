d508718f6f27261a2e80b27c8e7c7c89cededa24
David Ashford <ashford.david@gmail.com>
2018-05-19 17:44:13 +0100

    Test new sun tracker

--- src/Notification/NotificationManager.py
+++ src/Notification/NotificationManager.py
@@ -1,6 +1,9 @@
 import datetime
+import os
+
 import pytz
-from astral import Astral
+
+from sunrise_sunset.sunrise_sunset import SunriseSunset
 
 
 class NotificationManager:
@@ -9,19 +12,22 @@ class NotificationManager:
         self._allows = configuration['allow']
         self._denies = configuration['deny']
 
-        self._astral = Astral()
-        self._astral.solar_depression = 'civil'
-        self._city = self._astral['Dublin']
+        self._latitude = float(os.getenv('LOCATION_LATITUDE'))
+        self._longitude = float(os.getenv('LOCATION_LONGITUDE'))
 
     def is_satisfied(self):
         pass
 
     def _is_after_sunrise(self):
-        now = pytz.timezone('Europe/Dublin').localize(datetime.datetime.now())
-        sun = self._city.sun(local=True)
-        return now > sun['sunrise']
+        sun = SunriseSunset(
+            pytz.timezone('Europe/Dublin').localize(datetime.datetime.now()),
+            latitude=self._latitude,
+            longitude=self._longitude
+        )
+        # sun = self._city.sun(local=True)
+        # return now > sun['sunrise']
 
     def _is_after_sunset(self):
         now = pytz.timezone('Europe/Dublin').localize(datetime.datetime.now())
-        sun = self._city.sun(local=True)
-        return now > sun['sunset']
+        # sun = self._city.sun(local=True)
+        # return now > sun['sunset']
