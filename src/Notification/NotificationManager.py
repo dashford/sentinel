e17781c1ac5551e1a187c86929171952414ed2ce
David Ashford <ashford.david@gmail.com>
2018-05-27 11:30:21 +0100

    debug

--- src/Notification/NotificationManager.py
+++ src/Notification/NotificationManager.py
@@ -15,12 +15,14 @@ class NotificationManager:
 
     def is_satisfied(self):
         for denied in self._denies:
+            print(denied)
             if denied == 'after_sunset' and self._is_after_sunset():
                 return False
             if denied == 'all':
                 return False
 
         for allowed in self._allows:
+            print(allowed)
             if allowed == 'after_sunrise' and self._is_after_sunrise():
                 return True
 
