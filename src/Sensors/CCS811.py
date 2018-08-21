77bfd01d2a0bcfb3afe0ef30491928fcf094cb89
David Ashford <ashford.david@gmail.com>
2018-08-21 20:54:18 +0100

    CCS811 eCO2 measurement

--- src/Sensors/CCS811.py
+++ src/Sensors/CCS811.py
@@ -2,6 +2,7 @@ import logging
 from time import sleep
 
 from Adafruit_CCS811 import Adafruit_CCS811
+from blinker import signal
 
 
 class CCS811:
@@ -9,12 +10,14 @@ class CCS811:
         logging.debug('Initialising CCS811 sensor with address {}'.format(address))
         self._sensor = Adafruit_CCS811(address=address)
 
-    def get_equivalent_co2(self):
+    def get_equivalent_co2(self, mqtt_details):
         """
         Return measured equivalent carbon dioxide from the device.
 
+        :param dict mqtt_details: Relevant details for publishing to the MQTT broker
         :return float:
         """
+        logging.debug('Measuring equivalent CO2')
         eco2 = None
         pending_measurement = True
 
@@ -22,11 +25,13 @@ class CCS811:
             if self._sensor.available():
                 eco2 = self._sensor.geteCO2()
                 pending_measurement = False
+                logging.info('eCO2 received from sensor: {}'.format(eco2))
+            logging.debug('Sensor data not ready yet, will try again...')
             sleep(0.5)
 
-        logging.info('Returning eco2: {}'.format(eco2))
-
-        return eco2
+        logging.info('Broadcasting eCO2: {}'.format(eco2))
+        temperature_signal = signal('eco2')
+        temperature_signal.send(self, eco2=eco2, mqtt_topic=mqtt_details['topic'])
 
     def get_air_quality(self):
         """
