c67adde67ac0e91a1cef6f6ce32dc3be30b951ea
David Ashford <david@dashford.io>
2020-11-28 21:56:17 +0000

    add more logging

--- src/Devices/Sensors/PMS5003.py
+++ src/Devices/Sensors/PMS5003.py
@@ -28,9 +28,18 @@ class PMS5003:
             pm10_0.append(data.pm_ug_per_m3(size=None, atmospheric_environment=True))
             sleep(1)
 
+        avg_1_0 = sum(pm1_0) / len(pm1_0)
+        avg_2_5 = sum(pm2_5) / len(pm2_5)
+        avg_10_0 = sum(pm10_0) / len(pm10_0)
+
+        logging.info('Broadcasting pm1.0_ug/m3: {}'.format(avg_1_0))
         pm1_0_signal = signal('pm1.0_ug/m3')
-        pm1_0_signal.send(self, pm=sum(pm1_0) / len(pm1_0), mqtt_topic=mqtt_details['topic'] + '/1.0')
+        pm1_0_signal.send(self, pm=avg_1_0, mqtt_topic=mqtt_details['topic'] + '/1.0')
+
+        logging.info('Broadcasting pm2.5_ug/m3: {}'.format(avg_2_5))
         pm2_5_signal = signal('pm2.5_ug/m3')
-        pm2_5_signal.send(self, pm=sum(pm2_5) / len(pm2_5), mqtt_topic=mqtt_details['topic'] + '/2.5')
+        pm2_5_signal.send(self, pm=avg_2_5, mqtt_topic=mqtt_details['topic'] + '/2.5')
+
+        logging.info('Broadcasting pm10.0_ug/m3: {}'.format(avg_10_0))
         pm10_0_signal = signal('pm10.0_ug/m3')
-        pm10_0_signal.send(self, pm=sum(pm10_0) / len(pm10_0), mqtt_topic=mqtt_details['topic'] + '/10.0')
+        pm10_0_signal.send(self, pm=avg_10_0, mqtt_topic=mqtt_details['topic'] + '/10.0')
