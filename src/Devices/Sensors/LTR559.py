8b51e515062627f344a6a6241cf5e18a103edcbe
David Ashford <david@dashford.io>
2020-11-28 21:44:29 +0000

    use default address

--- src/Devices/Sensors/LTR559.py
+++ src/Devices/Sensors/LTR559.py
@@ -7,7 +7,7 @@ from blinker import signal
 class LTR559:
     def __init__(self, address):
         logging.info('Initialising LTR559 sensor with address {}'.format(address))
-        self._sensor = ltr559.LTR559(i2c_dev=address)
+        self._sensor = ltr559.LTR559()
 
     def get_lux(self, mqtt_details):
         """
